22a33e56db05711e382073c776e328572a4f94ae
Yana_Gracheva <push1022@ya.ru>
2022-12-06 00:37:57 +0300

    ДЗ по 7 семинару

---
+++ Task50/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 - > такого числа в массиве нет
+
+int rowsX = ReadLineToInt("Введите индекс строки искомого числа Х: ");
+int columsX = ReadLineToInt("Введите индекс столбца искомого числа Х: ");
+int[,] array = GetArrayRandom(5, 5);
+PrintArray(array);
+
+int[,] GetArrayRandom(int rows, int cols, int minValue = 0, int maxValue = 10)
+{
+    int[,] array = new int[rows, cols];
+    var rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(minValue, maxValue + 1);
+
+        }
+
+    }
+    return array;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Поиск числа в массиве по заданному индексу строки и столбца
+if (rowsX < array.GetLength(0) && columsX < array.GetLength(1))
+    Console.WriteLine($" Х ( {rowsX};{columsX} ) равен {array[rowsX, columsX]}");
+else
+    Console.WriteLine($" Х ( {rowsX};{columsX} ) -> такого числа в массиве нет");
+
+
+// Функция приема данных от пользователем с преобразованием в тип данных int 
+int ReadLineToInt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
---
+++ Task52/Program.cs
@@ -0,0 +1,61 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел.
+//Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] array = new int[4, 5];
+CreateArray(array);
+PrintArray2D(array);
+
+double[] Average = new double[array.GetLength(1)];
+
+for (int i = 0; i < array.GetLength(1); i++)
+{
+    double result = 0.0;
+    for (int j = 0; j < array.GetLength(0); j++)
+    {
+        result += array[j, i];
+    }
+    Average[i] = result / array.GetLength(0);
+}
+PrintArray(Average);
+
+
+// Заполнение массива числами от 0 до 100
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 100);
+        }
+    }
+}
+
+//  Функция вывода двумерного массива в терминал
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+// Функция
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + "; ");
+    }
+    Console.WriteLine();
+}
